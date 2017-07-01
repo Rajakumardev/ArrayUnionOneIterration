0ded1f35251bc43b95870e56106d61a7cc9d1a7e
Rajakumar <rajakumarofficial@gmail.com>
2017-07-01 16:23:56 +0530

    Double digit bug fixed
    Double digit bug is fixed by adding extra white spaces before and after the individual number(> 1 digit).

--- src/MainClass.java
+++ src/MainClass.java
@@ -5,22 +5,22 @@ public class MainClass {
 		System.out.println(data);
 	}
 	public static void main(String args[]){
-		int a[] = new int[] {1,2,3,5,6};
-		int b[]  = new int[] {4,2,1,7,3,6,7,5,5,4,4,6,6,6};
+		int a[] = new int[] {11,22,33,5,6};
+		int b[]  = new int[] {4,2,1,7,3};
 		int res[] = new int[a.length+b.length];
 		int disp = 0;
 		String tmp ="";
 		for(int i=0;i<(b.length+a.length);i++){
 			if(i < a.length){
-				if(tmp.indexOf(""+a[i])==-1){
+				if(tmp.indexOf(" "+a[i]+" ")!=0){
 					res[i-disp] = a[i];
-					tmp = tmp+a[i];
+					tmp = tmp+" "+a[i];
 				}else{
 					disp++;
 				}
 			}else{
-				if(tmp.indexOf(""+b[i-a.length]) == -1){
-					tmp = tmp + b[i-a.length];
+				if(tmp.indexOf(" "+b[i-a.length]+" ") ==-1){
+					tmp = tmp +" "+ b[i-a.length];
 					res[i-disp] = b[i-a.length];
 				}else{
 					disp++;
